bb536f28fa85bdcb77d12d6716994ea039d1285a
pororoJ <walkslowlyjia@gmail.com>
2017-03-24 00:17:33 +0800

    【story=66】个人信息页面内容修改

--- app/src/components/content/personalcenter/index.js
+++ app/src/components/content/personalcenter/index.js
@@ -9,15 +9,25 @@ class Personal extends Component {
                 <div className="mask"></div>
                 <form className="personal-form">
                     <div className="form-item">
-                        <label htmlFor="" className="personal-label">手机号:</label>
-                        <input className="personal-input" type="text"/>
+                        <p  className="personal-label">用户名:</p>
+                        <p className="personal-detail">user</p>
                     </div>
                     <div className="form-item">
-                        <label htmlFor="" className="personal-label">密码:</label>
-                        <input className="personal-input" type="password"/>
+                        <p className="personal-label">手机号:</p>
+                        <p className="personal-detail">18332518016</p>
                     </div>
                     <div className="form-item">
-                        <button className="create-user">保存</button>
+                        <p className="personal-label">密码:</p>
+                        <div className="personal-password" style={{display:'none'}}>
+                            <p className="personal-detail">**********</p>
+                            <button>修改密码</button>
+                        </div>
+                        <div>
+                            <input type="password" className="personal-password"/>
+                        </div>
+                    </div>
+                    <div className="form-item">
+                        <button className="save-password">保存</button>
                     </div>
                 </form>
             </div>
